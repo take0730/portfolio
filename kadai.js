582d6714e27951949b389e431adb0132481d6b5c
take0730 <x20052xx@aitech.ac.jp>
2021-07-26 02:55:07 +0900

    更新

--- kadai.js
+++ kadai.js
@@ -1,17 +1,8 @@
-function hamburger() {
-    document.getElementById('line1').classList.toggle('line_1');
-    document.getElementById('line2').classList.toggle('line_2');
-    document.getElementById('line3').classList.toggle('line_3');
-    document.getElementById('menu').classList.toggle('in');
-  }
-  document.getElementById('hamburger').addEventListener('click' , function () {
-    hamburger();
-  } );
 
-  const image01src = 'url("img/kadai_01.jpeg")';
-  const image02src = 'url("img/kadai_02.jpeg")';
-  const image03src = 'url("img/kadai_03.jpeg")';
-
+  const image01src = 'url("img/image_1-01.png")';
+  const image02src = 'url("img/image_2.png")';
+  const image03src = 'url("img/image_03.png")';
+  const image04src = 'url("img/image_04.png")';
 function nextImage() {
   const header = document.getElementById('top');
   const nowBg = header.style.backgroundImage;
@@ -23,8 +14,11 @@ function nextImage() {
   } else if (nowBg == image02src) {
     // 今2枚め
     header.style.backgroundImage = image03src;
+  }else if (nowBg == image03src) {
+    // 今2枚め
+    header.style.backgroundImage = image04src;
   } else {
-    // 今3枚め
+    // 今4枚め
     header.style.backgroundImage = image01src;
   }
 }
@@ -36,13 +30,16 @@ function prevImage() {
 
   if (nowBg == image01src) {
     // 今1枚め TODO:
-    header.style.backgroundImage = image03src;
+    header.style.backgroundImage = image04src;
   } else if (nowBg == image02src) {
     // 今2枚め TODO:
     header.style.backgroundImage = image01src;
-  } else {
+  }else if (nowBg == image03src) {
     // 今3枚め TODO:
     header.style.backgroundImage = image02src;
+  } else {
+    // 今4枚め TODO:
+    header.style.backgroundImage = image03src;
   }
 }
 
@@ -54,23 +51,4 @@ document.getElementById('sc-to-right').addEventListener('click', nextImage);
 // タイマーセット
 setInterval(nextImage, 5000);
 
-
-function popupImage() {
-    var popup = document.getElementById('js-popup');
-    if(!popup) return;
   
-    var blackBg = document.getElementById('js-black-bg');
-    var closeBtn = document.getElementById('js-close-btn');
-    var showBtn = document.getElementById('js-show-popup');
-  
-    closePopUp(blackBg);
-    closePopUp(closeBtn);
-    closePopUp(showBtn);
-    function closePopUp(elem) {
-      if(!elem) return;
-      elem.addEventListener('click', function() {
-        popup.classList.toggle('is-show');
-      });
-    }
-  }
-  popupImage();
